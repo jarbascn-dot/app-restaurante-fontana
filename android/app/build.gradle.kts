0823c8289433d559ab2130db301828b685201c0a
jarbascn-dot <jarbascn@gmail.com>
2026-07-07 18:12:39 -0300

    chore: bump versionCode to 3 / versionName to 1.1.1 for new release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ defaultConfig {
 applicationId = "com.fontana.sgr"
 minSdk = 26
 targetSdk = 35
-versionCode = 2
-versionName = "1.1.0"
+versionCode = 3
+    versionName = "1.1.1"
 }
 
 signingConfigs {
