f0029a36200da136ed90234a12b44af07950d8e5
jarbascn-dot <jarbascn@gmail.com>
2026-07-12 21:05:59 -0300

    Bump versionCode/versionName para 8/1.1.6 (correção da vírgula)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ defaultConfig {
 applicationId = "com.fontana.sgr"
 minSdk = 26
 targetSdk = 35
-    versionCode = 7
-    versionName = "1.1.5"
+    versionCode = 8
+    versionName = "1.1.6"
 }
 
 signingConfigs {
