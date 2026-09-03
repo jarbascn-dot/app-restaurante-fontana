cc3cd2a8c878b49e755bcee23781f1ae5cbb6986
jarbascn-dot <jarbascn@gmail.com>
2026-09-03 08:45:59 -0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ defaultConfig {
 applicationId = "com.fontana.sgr"
 minSdk = 26
 targetSdk = 36
-        versionCode = 26
-        versionName = "1.1.24"
+        versionCode = 27
+        versionName = "1.1.25"
     }
 
 signingConfigs {
